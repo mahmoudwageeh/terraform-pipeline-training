7fb2b068d521b572e33dadb25842e56b2a659272
mahmoud.w94 <mahmoud.w94@gmail.com>
2024-11-01 13:32:54 +0200

    updating client id, secret id, subscription id, tenant id in yaml file

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ provider "azurerm" {
    features {}
    subscription_id = "b98d2b8d-4039-4e94-a202-67c3a82a3ced"
    tenant_id = "17643a2b-8c04-48b3-89ef-d5149a115c30"
-
+   client_id       = "101b290d-a2af-4288-8f6e-2d129d548042"
+   client_secret   = "Ma68Q~bgfcU0M6BepLSaxSOmPO5sBro1UxThXcXP"
 }
 
 
