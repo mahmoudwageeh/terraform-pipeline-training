a691154d08cb0ab30692adca448aafa30ce3ff6f
Mahmoud Wageeh <mahmoud.w94@gmail.com>
2024-11-17 16:31:39 +0200

    deleting backend.tf file

--- backend.tf
+++
@@ -1,9 +0,0 @@
-#
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "Terraform-RG"
-#    storage_account_name = "terraformsa94"
-#    container_name       = "tfstate"
-#    key                  = "test.terraform.tfstate"
-#  }
-#}
