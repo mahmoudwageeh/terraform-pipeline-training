374bdb38011eacd2facc1b6bdaec1125eb3946a1
Mahmoud Wageeh <mahmoud.w94@gmail.com>
2024-11-17 21:43:53 +0200

    removing backend section from azure

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@ terraform {
       version = "=3.0.0"
     }
   }
-
+  backend "azurerm" {
+    resource_group_name  = "Terraform-RG"
+    storage_account_name = "terraformsa94"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+  }  
 }
 
 
