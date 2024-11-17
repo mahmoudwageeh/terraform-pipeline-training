357535def420b496d0eebc84fd28e4482477d2d3
Mahmoud Wageeh <mahmoud.w94@gmail.com>
2024-11-17 21:59:27 +0200

    removing backend section from azure and creating new RG

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example2" {
   name     = "Terraform-RG3"
   location = "West Europe"
 }
